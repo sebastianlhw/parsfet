f5beed14189541a5e714bf734ca644e69c68299d
sebastianl <sebastianlhw@users.noreply.github.com>
2026-01-25 20:54:12 +0000

    Update cell logic to also use lark parser

--- src/parsfet/normalizers/__init__.py
+++ src/parsfet/normalizers/__init__.py
@@ -1,8 +1,14 @@
 """Normalizers for cross-library comparison"""
 
+from .classifier import CellType, classify_cell, classify_function
+from .classifier_legacy import classify_function as classify_function_legacy
 from .invd1 import INVD1Normalizer, NormalizedMetrics
 
 __all__ = [
+    "CellType",
+    "classify_cell",
+    "classify_function",
+    "classify_function_legacy",
     "INVD1Normalizer",
     "NormalizedMetrics",
 ]
