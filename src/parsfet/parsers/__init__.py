d47565a5a96fc3ca285f3cdfe71b727c2267557a
sebastianl <sebastianlhw@users.noreply.github.com>
2026-02-05 23:34:56 +0000

    remove legacy code

--- src/parsfet/parsers/__init__.py
+++ src/parsfet/parsers/__init__.py
@@ -2,18 +2,13 @@
 
 from .base import BaseParser
 from .lef import LEFParser, TechLEFParser
-from .lef_legacy import LegacyLEFParser, LegacyTechLEFParser
 from .liberty import LibertyParser
 from .liberty_json import LibertyJSONParser
-from .liberty_legacy import LegacyLibertyParser
 
 __all__ = [
     "BaseParser",
     "LibertyParser",
-    "LegacyLibertyParser",
     "LibertyJSONParser",
     "LEFParser",
-    "LegacyLEFParser",
     "TechLEFParser",
-    "LegacyTechLEFParser",
 ]
